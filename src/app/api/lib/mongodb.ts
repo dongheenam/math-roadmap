6a954d58c6817bda6ab747250a18a9acf43054fc
Donghee Nam <57467269+dongheenam@users.noreply.github.com>
2023-04-02 19:28:22 +1000

    Improve Mongo connector

--- src/app/api/lib/mongodb.ts
+++ src/app/api/lib/mongodb.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb';
 
 // ref: https://github.com/vercel/next.js/blob/canary/examples/with-mongodb/lib/mongodb.ts
+// ref: https://www.mongodb.com/docs/atlas/manage-connections-aws-lambda/
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
@@ -8,27 +9,23 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
-let clientPromise: Promise<MongoClient>;
+let client: MongoClient;
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>;
+    _mongoClient?: MongoClient;
   };
 
-  if (!globalWithMongo._mongoClientPromise) {
+  if (!globalWithMongo._mongoClient) {
     client = new MongoClient(uri, options);
-    globalWithMongo._mongoClientPromise = client.connect();
+    globalWithMongo._mongoClient = client;
   }
-  clientPromise = globalWithMongo._mongoClientPromise;
+  client = globalWithMongo._mongoClient;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
-  clientPromise = client.connect();
 }
 
-// Export a module-scoped MongoClient promise. By doing this in a
-// separate module, the client can be shared across functions.
-export default clientPromise;
+export default client;
--- src/app/api/skills/route.ts
+++ src/app/api/skills/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from 'next/server';
 
-import clientPromise from '../lib/mongodb';
+import client from '../lib/mongodb';
 
 export async function GET(request: Request) {
-  const client = await clientPromise;
   const data = await client
     .db('math-roadmap-main')
     .collection('skills')
