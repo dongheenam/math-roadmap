b139cf96637d070c09a6424530472539b2e2d358
Donghee Nam <57467269+dongheenam@users.noreply.github.com>
2023-04-13 10:40:34 +1000

    Load the prerequisite skills automatically

--- src/app/skills/[_id]/Prerequisites.tsx
+++ src/app/skills/[_id]/Prerequisites.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
 import { PlainSkill } from '@/app/types/skills';
@@ -8,23 +8,28 @@ import { PlainSkill } from '@/app/types/skills';
 type Props = { _id: string };
 type SkillWithDepth = PlainSkill & { depth: number };
 
+const load = async (_id: string) => {
+  const res = await fetch(`/api/skills/${_id}/prerequisites`);
+
+  if (!res.ok) {
+    throw new Error('Failed to load prerequisites');
+  }
+
+  // Easier skills have a higher depth
+  const data = (await res.json()) as SkillWithDepth[];
+  return data.sort((a, b) => b.depth - a.depth);
+};
+
 export default function Prerequisites({ _id }: Props) {
   const [loaded, setLoaded] = useState(false);
   const [prerequisites, setPrerequisites] = useState<SkillWithDepth[]>([]);
 
-  const load = async () => {
-    if (loaded) return;
-    const res = await fetch(`/api/skills/${_id}/prerequisites`);
-
-    if (!res.ok) {
-      throw new Error('Failed to load prerequisites');
-    }
-
-    // Easier skills have a higher depth
-    const data = (await res.json()) as SkillWithDepth[];
-    setPrerequisites(data.sort((a, b) => b.depth - a.depth));
-    setLoaded(true);
-  };
+  useEffect(() => {
+    load(_id).then((data) => {
+      setPrerequisites(data);
+      setLoaded(true);
+    });
+  }, [_id]);
 
   const prerequisitesView =
     prerequisites.length === 0 ? (
@@ -40,7 +45,7 @@ export default function Prerequisites({ _id }: Props) {
   return (
     <>
       <h3>Prerequisites</h3>
-      {!loaded && <button onClick={() => load()}>Load</button>}
+      {!loaded && <p>Loading prerequisites...</p>}
       <ol>{loaded && prerequisitesView}</ol>
     </>
   );
